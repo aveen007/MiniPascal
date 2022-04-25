3debdf72ba824812a51104633d6115b778307b45
aveen007 <aveen2000hussein@gmail.com>
2022-04-25 14:10:47 +0200

    comm

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+extern int yyparse();
+extern int yyerror(const char *);
+
+int main()
+{
+    yyparse();
+    system("pause");
+    return 0;
+}
+// ifiuisdbo
+
+// here
