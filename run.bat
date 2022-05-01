d9ee0227c737bde6b9e035a0b8f6093ce1a3236d
aveen007 <aveen2000hussein@gmail.com>
2022-05-01 12:49:44 +0200

    del app.exe trouble

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 flex.exe -oScanner.cpp scanner.l
 bison.exe -dtv -o Parser.cpp --define=Parser.h Parser.y
+del app.exe
 g++ Parser.cpp Scanner.cpp main.cpp Parser.h -o app
 :: You can comment the followin line
 del Scanner.cpp Parser.cpp Parser.output Parser.h
